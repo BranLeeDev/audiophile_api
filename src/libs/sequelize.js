5ae4f4a2dbd149b236eb396ab7828eaf6f7ebd80
Brandon <brandonaguerodeveloper@gmail.com>
2023-10-16 05:03:25 -0500

    Declaramos nuestras constantes USER y PASSWORD asignadoles su valores, para usarlos en nuestro URI

--- src/libs/sequelize.js
+++ src/libs/sequelize.js
@@ -6,6 +6,9 @@ let URI;
 if (config.isProd) {
   URI = config.dbUrl;
 } else {
+  const USER = encodeURIComponent(config.dbUser);
+  const PASSWORD = encodeURIComponent(config.dbPassword);
+
   URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 }
 
